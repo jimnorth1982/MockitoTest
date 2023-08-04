d3bfc52bf6b083fca14981ed80beb0ab99da3802
Jimi N <jimnorth82@icloud.com>
2023-08-04 08:04:47 -0400

    Initial commit

---
+++ src/main/java/org/jimnorth1982/mock/App.java
@@ -0,0 +1,13 @@
+package org.jimnorth1982.mock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
